318382871b85e3e2a78a60b41dbb2af2c90d7627
oussama-soulimani <64189972+oussama-soulimani@users.noreply.github.com>
2023-02-02 12:46:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-// if (‘serviceWorker’ in navigator) {
-//   navigator.serviceWorker.register(‘/sw.js’, { scope: ‘/personen/’ } );
-// }
+if (‘serviceWorker’ in navigator) {
+  navigator.serviceWorker.register(‘/sw.js’, { scope: ‘/personen/’ } );
+}
 
-// if ('pushManager' in window){
-//   Notification.requestPermission().then(function(result) {
-//   if (result === 'denied') {
-//     console.log('Geen toestemming gekregen, later nog eens proberen?');
-//     return;
-//   }
-//   if (result === 'default') {
-//     console.log('De toestemming is afgewezen.');
-//     return;
-//   }
-//   console.log("Permission Granted!");
-// });
-// }
-
-console.log("test");
+if ('pushManager' in window){
+  Notification.requestPermission().then(function(result) {
+  if (result === 'denied') {
+    console.log('Geen toestemming gekregen, later nog eens proberen?');
+    return;
+  }
+  if (result === 'default') {
+    console.log('De toestemming is afgewezen.');
+    return;
+  }
+  console.log("Permission Granted!");
+});
+}
