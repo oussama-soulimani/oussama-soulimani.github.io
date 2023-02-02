48cd7d21bb984d61901e6492774baa542323872f
oussama-soulimani <64189972+oussama-soulimani@users.noreply.github.com>
2023-02-02 12:48:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,16 +2,16 @@ if (‘serviceWorker’ in navigator) {
   navigator.serviceWorker.register(‘/sw.js’, { scope: ‘/personen/’ } );
 }
 
-if ('pushManager' in window){
-  Notification.requestPermission().then(function(result) {
-  if (result === 'denied') {
-    console.log('Geen toestemming gekregen, later nog eens proberen?');
-    return;
-  }
-  if (result === 'default') {
-    console.log('De toestemming is afgewezen.');
-    return;
-  }
-  console.log("Permission Granted!");
-});
-}
+// if ('pushManager' in window){
+//   Notification.requestPermission().then(function(result) {
+//   if (result === 'denied') {
+//     console.log('Geen toestemming gekregen, later nog eens proberen?');
+//     return;
+//   }
+//   if (result === 'default') {
+//     console.log('De toestemming is afgewezen.');
+//     return;
+//   }
+//   console.log("Permission Granted!");
+// });
+// }
